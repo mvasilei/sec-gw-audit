7c3012ef63799aa7077840def61b32501a762ded
mvasilei <53435274+mvasilei@users.noreply.github.com>
2019-10-10 14:01:49 +0100

    Update sec-gw-checks.py

--- sec-gw-checks.py
+++ sec-gw-checks.py
@@ -5,17 +5,16 @@ show_interface_counters_errors = 'sho int counters errors | exclude  0          
 show_interface_transceiver = 'sho interfaces transceiver\n'
 show_route_map = 'sho route-map ENCR\n'
 show_ip_access_list = 'sho ip access-lists | sec Extended IP access list 10.\n'
+show_ip_route = 'show ip route\n'
 
 device_list_odd = ['XAN-CA-01','XCY-CA-01','XEH-CA-01','XER-CA-01','XGL-CA-01','XGR-CA-01',
 			 'XKS-CA-01','XLD-CA-01','XMS-CA-01','XOM-CA-01','XRM-CA-01','XSN-CA-01','XSY-CA-01']
 device_list_even = ['XAN-CA-02','XCY-CA-02','XEH-CA-02','XER-CA-02','XGL-CA-02','XGR-CA-02',
 			 'XKS-CA-02','XLD-CA-02','XMS-CA-02','XOM-CA-02','XRM-CA-02','XSN-CA-02','XSY-CA-02']
-			 
-USER = raw_input('Username: ')
-PASS = getpass.getpass(prompt='Password: ')
-def execute_command():
+
+def connection_establishment():
    try:
-      print 'Processing HOST: 'host
+      print 'Processing HOST: ', host
       client = paramiko.SSHClient()
       client.load_system_host_keys()
       client.set_missing_host_key_policy(paramiko.WarningPolicy())
@@ -24,35 +23,28 @@ def execute_command():
       channel = client.invoke_shell()
       
       channel.send('term len 0\n')
-      channel.send(show_interface_counters_errors)
-      time.sleep(2)
-      out1 = channel.recv(65535)
-      channel.send(show_interface_transceiver)
-      time.sleep(2)
-      out2 = channel.recv(65535)
-      channel.send(show_route_map)
-      time.sleep(2)
-      out3 = channel.recv(65535)
-      channel.send(show_ip_access_list)
-      time.sleep(2)
-      out4 = channel.recv(65535)
+      execute_command(show_interface_counters_errors, channel)
+      execute_command(show_interface_transceiver, channel)
+      execute_command(show_route_map, channel)
+      execute_command(show_ip_access_list, channel)
+      execute_command(show_ip_route, channel)
+
    finally:
       client.close()
 
-   return(out1,out2,out3,out4)
+def execute_command(command, channel):
+      channel.send(command)
+      time.sleep(2)
+      out = channel.recv(65535)
+      file.write(out)
+
+USER = raw_input('Username: ')
+PASS = getpass.getpass(prompt='Password: ')
 
 with open('even.log', 'w') as file:
    for host in device_list_even:
-      out1,out2,out3,out4 = execute_command()
-      file.write(out1)
-      file.write(out2)
-      file.write(out3)
-      file.write(out4)
+      connection_establishment()
 
 with open('odd.log', 'w') as file:
    for host in device_list_odd:
-      out1,out2,out3,out4 = execute_command()
-      file.write(out1)
-      file.write(out2)
-      file.write(out3)
-      file.write(out4)
+      connection_establishment()
