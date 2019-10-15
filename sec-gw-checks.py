336ba37043afddec29ddf203686b3250bc2faccc
mvasilei <53435274+mvasilei@users.noreply.github.com>
2019-10-15 08:32:37 +0100

    Update sec-gw-checks.py

--- sec-gw-checks.py
+++ sec-gw-checks.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
-import getpass, paramiko, time
+import getpass, paramiko, time, re
 
 show_interface_counters_errors = 'sho int counters errors | exclude  0           0            0            0          0|0          0          0          0         0           0|0         0         0          0         0         0         0\n'
 show_interface_transceiver = 'sho interfaces transceiver\n'
 show_route_map = 'sho route-map ENCR\n'
-show_ip_access_list = 'sho ip access-lists | sec Extended IP access list 10.\n'
+show_ip_access_list = 'sho ip access-lists | sec Extended IP access list 1[01].\n'
 show_ip_route = 'show ip route\n'
+show_module = 'show module\n'
 
 device_list_odd = ['XAN-CA-01','XCY-CA-01','XEH-CA-01','XER-CA-01','XGL-CA-01','XGR-CA-01',
 			 'XKS-CA-01','XLD-CA-01','XMS-CA-01','XOM-CA-01','XRM-CA-01','XSN-CA-01','XSY-CA-01']
@@ -18,35 +19,73 @@ def connection_establishment():
       client = paramiko.SSHClient()
       client.load_system_host_keys()
       client.set_missing_host_key_policy(paramiko.WarningPolicy())
-
       client.connect(host, 22, username=USER, password=PASS)
       channel = client.invoke_shell()
-      
       channel.send('term len 0\n')
-      execute_command(show_interface_counters_errors, channel)
-      execute_command(show_interface_transceiver, channel)
-      execute_command(show_route_map, channel)
-      execute_command(show_ip_access_list, channel)
-      execute_command(show_ip_route, channel)
    except paramiko.AuthenticationException as error:
       print 'Authentication Error'
       exit()
-   finally:
-      client.close()
+   return (channel,client)
 
-def execute_command(command, channel):
-      channel.send(command)
-      time.sleep(2)
-      out = channel.recv(65535)
-      file.write(out)
+def connection_teardown(client):
+   client.close()
+
+def sami_count(out):
+   modules = []
+   for line in out.splitlines():
+       line = line.rstrip()
+       pattern = re.compile(r'^ *(?P<module>\d+)')
+       m = pattern.match(line)
+   
+       if m:
+          modules.append(m.group(0).strip())
+
+   return(modules)
+
+def execute_command(command, channel,wait):
+   channel.send(command)
+   time.sleep(wait)
+   out = channel.recv(65535)
+   file.write(out)
+   return (out)
 
 USER = raw_input('Username: ')
 PASS = getpass.getpass(prompt='Password: ')
 
-with open(time.strftime("%Y%m%d-%H%M%S")+'even.log', 'w') as file:
+with open(time.strftime("%Y%m%d-%H%M%S")+'-even.log', 'w') as file:
    for host in device_list_even:
-      connection_establishment()
+      channel,client = connection_establishment()
+      execute_command(show_interface_counters_errors, channel,2)
+      execute_command(show_interface_transceiver, channel,2)
+      execute_command(show_route_map, channel,2)
+      execute_command(show_ip_access_list, channel,2)
+      execute_command(show_ip_route, channel,2)
+      execute_command(show_module,channel,2)
+      out = execute_command('show module services\n',channel,2)
+      modules = sami_count(out)
 
-with open(time.strftime("%Y%m%d-%H%M%S")+'odd.log', 'w') as file:
+      for m in modules:
+         execute_command('session slot ' + m +' process 3\n',channel,5)
+         execute_command('show crypto throughput\n',channel,2)
+         execute_command('exit\n',channel,2)
+
+      connection_teardown(client)
+
+with open(time.strftime("%Y%m%d-%H%M%S")+'-odd.log', 'w') as file:
    for host in device_list_odd:
-      connection_establishment()
+      channel,client = connection_establishment()
+      execute_command(show_interface_counters_errors, channel,2)
+      execute_command(show_interface_transceiver, channel,2)
+      execute_command(show_route_map, channel,2)
+      execute_command(show_ip_access_list, channel,2)
+      execute_command(show_ip_route, channel,2)
+      execute_command(show_module,channel,2)
+      out = execute_command('show module services\n',channel,2)
+      modules = sami_count(out)
+
+      for m in modules:
+         execute_command('session slot ' + m +' process 3\n',channel,5)
+         execute_command('show crypto throughput\n',channel,2)
+         execute_command('exit\n',channel,2)
+
+      connection_teardown(client)
